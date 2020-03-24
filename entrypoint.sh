1f7fc26e927e9002b1a61de7aa9be888dc6fb351
Jan Alfred Richter <jan@pixelfusion.co.nz>
2020-03-24 14:31:38 +1300

    fix: debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,6 +9,7 @@ if [ -z $FILENAME ]; then
 fi
 
 while IFS='=' read -r -a var; do
-  echo "Setting ${var[0]} to ${var[1]}"
+  echo "Setting ${var[0]} to ${var[1]} "
+  echo ${var[1]} | wc -l
   sed -i "s|__${var[0]}__|${var[1]}|g" $FILENAME
 done < <(printenv)
